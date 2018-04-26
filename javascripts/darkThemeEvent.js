1617392d48b9bc13281c825a5fc5e524bacf3c80
Corey Lutrick <coreylutrick@gmail.com>
2018-04-25 19:34:55 -0700

    got event listener working, just have to add all the styling in the next ticket

--- javascripts/darkThemeEvent.js
+++ javascripts/darkThemeEvent.js
@@ -1,20 +1,21 @@
 
-// const checkbox = document.getElementById('darkTheme');
-// const body = document.getElementById('body');
+const checkbox = document.getElementById('darkTheme');
+const body = document.getElementById('body');
 
 const addCheckBoxEvent = () =>
 {
   console.log('poop');
-  // checkbox.addEventListener('change', () =>
-  // {
-  //   if (this.checked)
-  //   {
-  //     body.classList.add('venom');
-  //   } else
-  //   {
-  //     body.classList.remove('venom');
-  //   }
-  // });
+  checkbox.addEventListener('change', (e) =>
+  {
+    console.log(e);
+    if (e.target.checked)
+    {
+      body.classList.add('venom');
+    } else
+    {
+      body.classList.remove('venom');
+    }
+  });
 };
 
-module.export = addCheckBoxEvent;
+module.exports = addCheckBoxEvent;
