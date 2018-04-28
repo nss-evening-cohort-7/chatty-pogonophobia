1ea7cd508cb4bd645932a72bbae5c128d9a93cd9
Tom Dugan <thomas@aadwebb.com>
2018-04-28 12:47:22 -0500

    update aesthetics

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -5,8 +5,8 @@ const buildDomString = (messages) =>
   let domString = '';
   messages.forEach((message, index) => {
     domString += `<div data-message-id='${index}' class='messages'>`;
-    domString +=  `<p>${message.message}</p>`;
-    domString +=  `<button class='deleteBtn'>Delete</button>`;
+    domString +=  `<p>${message.message}  &nbsp;  <span class="btn btn-default glyphicon glyphicon-trash deleteBtn" aria-hidden="true"></span></p>`;
+    domString +=  ``;
     domString += `</div>`;
   });
   return domString;
